3cf9cf1bca914a2b26a503a3bba3353cc8a93dcf
Sebastian Tiede <info@sebastiantiede.de>
2015-10-14 08:46:35 +0200

    bower dependencies added

--- Plugin.php
+++ Plugin.php
@@ -7,14 +7,14 @@ class Plugin extends Base
 {
     public function initialize()
     {
-        $this->hook->on('template:layout:js', 'plugins/wysiwyg/Asset/js/toopay-bootstrap-markdown/bower_components/bootstrap/dist/js/bootstrap.min.js');
-        $this->hook->on('template:layout:js', 'plugins/wysiwyg/Asset/js/toopay-bootstrap-markdown/bower_components/markdown/lib/markdown.js');
-        $this->hook->on('template:layout:js', 'plugins/wysiwyg/Asset/js/toopay-bootstrap-markdown/js/bootstrap-markdown.js');
+        $this->hook->on('template:layout:js', 'plugins/wysiwyg/Asset/bower_components/bootstrap/dist/js/bootstrap.min.js');
+        $this->hook->on('template:layout:js', 'plugins/wysiwyg/Asset/bower_components/markdown/lib/markdown.js');
+        $this->hook->on('template:layout:js', 'plugins/wysiwyg/Asset/bower_components/bootstrap-markdown/js/bootstrap-markdown.js');
 
-        $this->hook->on('template:layout:css', 'plugins/wysiwyg/Asset/js/toopay-bootstrap-markdown/css/bootstrap-markdown.min.css');
-        $this->hook->on('template:layout:css', 'plugins/wysiwyg/Asset/css/font-awesome-4.4.0/css/font-awesome.min.css');
+        $this->hook->on('template:layout:css', 'plugins/wysiwyg/Asset/bower_components/bootstrap-markdown/css/bootstrap-markdown.min.css');
+        $this->hook->on('template:layout:css', 'plugins/wysiwyg/Asset/bower_components/font-awesome/css/font-awesome.min.css');
+
         $this->hook->on('template:layout:css', 'plugins/wysiwyg/Asset/css/wysiwyg.css');
-
         $this->hook->on('template:layout:js', 'plugins/wysiwyg/Asset/js/wysiwyg.js');
     }
 }
