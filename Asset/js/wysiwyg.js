68860a110f96d6508e1592cbee822072e628c520
Sebastian Tiede <info@sebastiantiede.de>
2015-10-14 23:01:20 +0200

    inlucde project description

--- Asset/js/wysiwyg.js
+++ Asset/js/wysiwyg.js
@@ -1,25 +1,25 @@
 $(window).load(function() {
     $.fn.WYSIWYG = function() {
-        var form_comment = this,
-            form_comment_clone = form_comment.clone(true),
-            form_comment_container = $('<div class="kanboard-bootstrap-markdown-container"></div>').append(form_comment_clone);
-        console.log(form_comment);
-        form_comment.closest('.form-tabs').find('>.form-tabs-nav').hide();
+        this.each(function() {
+            var form_comment = $(this),
+                form_comment_clone = form_comment.clone(true),
+                form_comment_container = $('<div class="kanboard-bootstrap-markdown-container"></div>').append(form_comment_clone);
 
-        form_comment.replaceWith(form_comment_container);
+            form_comment.closest('.form-tabs').find('>.form-tabs-nav').hide();
 
-        console.log(form_comment_clone.width());
+            form_comment.replaceWith(form_comment_container);
 
-        form_comment_clone.markdown({
-            autofocus:false,
-            savable:false,
-            iconlibrary:'fa',
-            width:form_comment_clone.width(),
-            resize:'horizontal'
-        }).css({width:'100%'});
+            form_comment_clone.markdown({
+                autofocus:false,
+                savable:false,
+                iconlibrary:'fa',
+                width:form_comment_clone.width(),
+                resize:'horizontal'
+            }).css({width:'100%'});
+        });
     };
 
-    $('#form-comment').WYSIWYG();
+    $('#form-comment,#form-description').WYSIWYG();
 
     $(document).on("click", ".popover", function() {
         console.log($('#popover-container'));
